26fa3469c89680ec98d6c5dc2dad1f3122e8ee82
Miguel <kevin.miguel@all-win.software>
2023-05-16 09:34:38 -0700

    Add Nuke to image loading

--- NetworkingExample/ui/HomeScreen/Cells/CharacterItemCell.swift
+++ NetworkingExample/ui/HomeScreen/Cells/CharacterItemCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Nuke
 
 class CharacterItemCell: UICollectionViewCell, NibLoadable {
 
@@ -13,17 +14,17 @@ class CharacterItemCell: UICollectionViewCell, NibLoadable {
     @IBOutlet weak var nameUILabel: UILabel!
     @IBOutlet weak var StatusAndSpeciesUILabel: UILabel!
     @IBOutlet weak var LastKnownLocationUILabel: UILabel!
-    @IBOutlet weak var FirstSeenInUILabel: UILabel!
+    @IBOutlet weak var originLocationUILabel: UILabel!
 
     func configure(character: Character) {
-      
+        
         nameUILabel.text = character.name
         StatusAndSpeciesUILabel.text = "\(character.status) - \(character.species)"
         LastKnownLocationUILabel.text = character.location.name
-        FirstSeenInUILabel.text = character.episode.first ?? "unknown"
+        originLocationUILabel.text = character.origin.name
+        
+        if let url = URL(string: character.image){
+            Nuke.loadImage(with: url, into: characterUIImageView)
+        }
     }
 }
-
-//if let firstSeenIn = character.episode.first {
-//    FirstSeenInUILabel.text = firstSeenIn
-//}
